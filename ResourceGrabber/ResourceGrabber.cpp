5ef7bedbbe2fe19cbfcf639c83b35a0c2bdcfcf7
ambiesoft <ambiesoft.trueff@gmail.com>
2018-10-19 02:26:53 +0900

    HighDPI .NET v4.7.2

--- ResourceGrabber/ResourceGrabber.cpp
+++ ResourceGrabber/ResourceGrabber.cpp
@@ -1,4 +1,3 @@
-// ResourceGrabber.cpp : メイン プロジェクト ファイルです。
 
 #include "stdafx.h"
 #include "FormMain.h"
@@ -8,11 +7,9 @@ using namespace ResourceGrabber;
 [STAThreadAttribute]
 int main(array<System::String ^> ^args)
 {
-	// コントロールが作成される前に、Windows XP ビジュアル効果を有効にします
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false); 
 
-	// メイン ウィンドウを作成して、実行します
 	Application::Run(gcnew FormMain());
 	return 0;
 }
